08bd05f4e7ea7251a0b89039bc311d86238a78c4
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-10 19:32:51 -0400

    Added post functionality for comments

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -14,13 +14,46 @@ router.post('/', (req, res) => {
             .catch(err => {
                 console.log(err);
                 res.status(500).json({
-                    error: "There was an error while saving the post to the database"
+                    error: "There was an error while saving the post to the database."
                 })
             })
     } else {
         res.status(400).json({
             errorMessage: "Please provide title and contents for the post."
         })
+    }
+})
+
+router.post('/:id/comments', (req, res) => {
+    const id = req.params.id;
+    const { text } = req.body;
+    const comment = { post_id: id, text: text}
+    if (!text) {
+        res.status(400).json({
+            errorMessage: "Please provide text for the comment."
+        })
+    } else {
+        Posts.findById(id)
+            .then(post => {
+                if (post.length === 0) {
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist."
+                    })
+                }
+            })
+            .catch(err => console.log(err));
+        Posts.insertComment(comment)
+            .then(comment_id => {
+                res.status(201).json({
+                    text: req.body.text
+                })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: "There was an error while saving this comment to the database."
+                })
+            })
     }
 })
 
