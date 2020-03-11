becaa64b2e61ce74afba5b5ea3e7fc696f750360
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-10 21:08:17 -0400

    Added put endpoint for posts

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -130,4 +130,31 @@ router.delete('/:id', (req, res) => {
         })
 })
 
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const post = req.body;
+    if (!post.title || !post.contents) {
+        res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+        })
+    } else {
+        Posts.update(id, post)
+            .then(records => {
+                if (records === 0) {
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist."
+                    })
+                } else {
+                    res.status(200).json(post)
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: "The post information could not be modified."
+                })
+            })
+    }
+})
+
 module.exports = router;
