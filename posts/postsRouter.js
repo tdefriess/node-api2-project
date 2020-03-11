c7af913ca4c13c3249e0314a478df9ced17015b7
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-10 20:44:17 -0400

    Endpoint to retrieve comments given a post ID added.

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -70,4 +70,44 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Posts.findById(id)
+        .then(post => {
+            if (post.length === 0 ) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            } else {
+                res.status(200).json(post)
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: "The post information could not be retrieved."
+            })
+        })
+})
+
+router.get('/:id/comments', (req, res) => {
+    const id = req.params.id;
+    Posts.findPostComments(id)
+        .then(comments => {
+            if (comments.length === 0) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            } else {
+                res.status(200).json(comments);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: "The comments information could not be retrieved."
+            })
+        })
+})
+
 module.exports = router;
